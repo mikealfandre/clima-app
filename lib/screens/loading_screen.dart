0b2a6dd9404760a282f2c8bfba39fb58b7f4dd59
Michael Alfandre <Michael Alfandre>
2019-06-12 19:37:08 -0600

    using dart decoder package to parse json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class LoadingScreen extends StatefulWidget {
@@ -26,8 +27,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data  = response.body;
+      print(data);
+
+      var longitude = jsonDecode(data)['coord']['lon'];
+      print(longitude);
+
+    } else {
+      print(response.statusCode);
+    }
+    
     print(response.body);
+
   }
 
   @override
